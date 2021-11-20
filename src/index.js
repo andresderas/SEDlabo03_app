be69b57dec93c89ccedddb616ce73ba997e93397
andresderas <00362718@uca.edu.sv>
2021-11-20 17:31:24 -0600

    Add: API Structure

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/tasks', require('./routes/task.routes'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')} `);
+});
