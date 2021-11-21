cbbdd5f50ecfd8a3490c3dfe24780dd08316669a
andresderas <00362718@uca.edu.sv>
2021-11-20 18:24:52 -0600

    Add: API routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'API Works!'
-    });
+const Task = require('../models/task');
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find()
+    console.log(tasks);
+    res.json(tasks); 
+});
+
+router.post('/', async (req, res) => {
+    const { name, username } = req.body;
+    const task = new Task({name, username});
+    console.log(task);
+    await task.save();
+    res.json({status: 'Task Saved'});
+});
+
+router.put('/:id', async (req, res) => {
+    const { name, username } = req.body;
+    const newTask = {name, username};
+    await Task.findByIdAndUpdate(req.params.id, newTask );
+    res.json({status: 'Task Updated'});
+
+});
+
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({status: 'Task Deleted'});
 });
 
 module.exports = router;
